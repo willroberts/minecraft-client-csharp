b4d788ca1010329e46faf7f46ec7fa75024d0a9f
Will Roberts <willroberts3@protonmail.com>
2021-03-13 11:30:27 -0500

    Adds Decode test

--- src/MinecraftClient.Tests/TestEncoder.cs
+++ src/MinecraftClient.Tests/TestEncoder.cs
@@ -26,5 +26,28 @@ namespace MinecraftClient.Tests.Encoder
 			};
 			Assert.AreEqual(bytes, expected);
 		}
+
+		[Test]
+		public void DecodeMessageTest()
+		{
+			byte[] bytes = new byte[] {
+				38, 0, 0, 0, //  Message length.
+				2, 0, 0, 0, // Message ID.
+				0, 0, 0, 0, // Message type.
+				83, 101, 101, 100, 58, 32, 91, 45, 50, 52, 55, 52, 49, 50, 53, 53, 55, 52, 56, 57, 48, 54, 57, 50, 51, 48, 56, 93, // Message body.
+				0, 0 // Terminator.
+			};
+			MinecraftClient.Message msg = MinecraftClient.Encoder.DecodeMessage(bytes);
+			MinecraftClient.Message expected = new MinecraftClient.Message(
+				38, // Message length.
+				2, // Message ID.
+				0, // Message type.
+				Encoding.ASCII.GetBytes("Seed: [-2474125574890692308]") // Message body.
+			);
+			Assert.AreEqual(msg.Length, expected.Length);
+			Assert.AreEqual(msg.ID, expected.ID);
+			Assert.AreEqual(msg.Type, expected.Type);
+			Assert.AreEqual(msg.Body, expected.Body);
+		}
 	}
 }
